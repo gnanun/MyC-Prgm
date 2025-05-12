b1e60be256978fc157dbe5f24dc7255e1f30dee3
gnanun <ggnani488@gmail.com>
2025-05-12 15:16:27 +0530

    Create sumoftwo.c

---
+++ sum of two.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+ int main()
+{
+    int a,b,sum;
+    printf("Enter the value of a and b:\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("Sum of %d and %d is:%d",a,b,sum);
+}
