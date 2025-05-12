00604d7c9c02da4411aa244b357ae921c73eb4ec
gnanun <ggnani488@gmail.com>
2025-05-12 16:02:09 +0530

    Create student details.c

---
+++ student details.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int age,usn;
+    char name, clg;
+    float weight,height;
+    printf("STUDENT DETAILS\n");
+    printf("Enter name:");
+    scanf("%s",&name);
+    printf("Enter your age:");
+    scanf("%d",&age);
+    printf("Enter your weight:");
+    scanf("%f",&weight);
+    printf("Enter your height:");
+    scanf("%f",&height);
+    printf("Enter USN:");
+    scanf("%d",&usn);
+    printf("Enter your clg name:");
+    scanf("%s",&clg);
+    return 0;
+}
