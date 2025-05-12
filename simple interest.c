a4a30e31d645543a6e7ff818e5540e4ec852aacb
gnanun <ggnani488@gmail.com>
2025-05-12 15:37:45 +0530

    Create simple interest.c

---
+++ simple interest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int amt,Time;
+    float roi,SI;
+    printf("Enter the  principle amount:");
+    scanf("%d",&amt);
+    printf("Enter time:");
+    scanf("%d",&Time);
+    printf("Enter rate of interest:");
+    scanf("%f",&roi);
+    SI=(amt*Time*roi)/100;
+    printf("Simple interest is:%.2f\n",SI);
+    
+    return 0;
+}
