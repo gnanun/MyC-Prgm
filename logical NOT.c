f0a5e4d87755d0bb1ffc59f508731678dde6a14f
GNANESH N <ggnani488@gmail.com>
2025-05-13 18:48:38 +0530

    Create logical NOT.c

---
+++ logical NOT.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int pass = 1;
+    if(!pass)
+    {
+        printf("You failed.\n");
+    }
+    else
+    {
+        printf("You passed.\n");
+    }
+    return 0;
+}
