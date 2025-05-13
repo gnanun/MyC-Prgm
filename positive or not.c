d4ffa9af69c3919d8babfdde8518bb37b7cbc1a5
GNANESH N <ggnani488@gmail.com>
2025-05-13 10:10:57 +0530

    Create positive or not.c

---
+++ positive or not.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+{
+    int a;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("%d is positive.");
+    }
+    return 0;
+}
