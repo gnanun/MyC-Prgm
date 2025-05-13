5698dfd62572145b80bcdbe4871b45fe1c6b3d7d
GNANESH N <ggnani488@gmail.com>
2025-05-13 19:29:35 +0530

    Create for loop.c

---
+++ for loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Hi Gnanu!\n");
+    printf("Enter number of terms:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
