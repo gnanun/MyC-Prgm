3109b7fbff1f22b0e41decd1faec74e6d083df6b
GNANESH N <ggnani488@gmail.com>
2025-05-13 12:29:46 +0530

    Create fibonacci using for.c

---
+++ fibonacci using for.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,t1=0,t2=1,t3;
+    printf("Enter the length of sequence to be generated:");
+    scanf("%d",&n);
+    printf("Fibonacci sequence is:%d,%d",t1,t2);
+    for(i=3;i<=n;i++)
+    {
+        t3=t1+t2;
+        printf(",%d",t3);
+        t1=t2;
+        t2=t3;
+    }
+}
