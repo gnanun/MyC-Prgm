6880b326da0773ba2c1f71be8d96299a540f9a5f
GNANESH N <ggnani488@gmail.com>
2025-05-13 19:39:53 +0530

    Create break.c

---
+++ break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Hi Gnanu!\n");
+    printf("Enter number of terms:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i==5)
+        break;
+        printf("%d ",i);
+    }
+    return 0;
+}
