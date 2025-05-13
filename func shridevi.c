5b79ecacb4b4373232ee797e0cc6bc105de5320e
GNANESH N <ggnani488@gmail.com>
2025-05-13 15:42:15 +0530

    Create func shridevi.c

---
+++ func shridevi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+char shridevi()
+{
+    char name[20]="Gnanu";
+    int D=16,M=06,Y=2007,USN =43;
+    long int num=7676285059;
+    printf("Name:%s\n",name);
+    printf("USN:%d\n",USN);
+    printf("Date of Birth:%d-%d-%d\n",D,M,Y);
+    printf("Phone Number:%ld\n",num);
+}
+ int main()
+ {
+     shridevi();
+     return 0;
+ }
