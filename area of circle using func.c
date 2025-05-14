133a5664c4812f5b78e15c018da1f28afdcd57f0
GNANESH N <ggnani488@gmail.com>
2025-05-14 18:23:58 +0530

    Create area of circle using func.c

---
+++ area of circle using func.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int r;
+int areaofcir()
+{
+    float pi=3.14,area;
+    area=pi*r*r;
+    return area;
+}
+int main()
+{
+    printf("hi Gnanu!\n");
+    printf("Enter the radius of circle:");
+    scanf("%d",&r);
+    float  result=areaofcir(r);
+    printf("Area of circle is:%.2f.",result);
+}
