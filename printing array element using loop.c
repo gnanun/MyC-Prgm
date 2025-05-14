745e97eebfea18a3cca8efd7524ee0788d242b0f
GNANESH N <ggnani488@gmail.com>
2025-05-14 18:30:42 +0530

    Create printing array elements using loop.c

---
+++ printing array element using loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5]={10,20,30,40,50},i;
+    printf("hi Gnanu!\n");
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
