39d122ee6fa918b2baeecf2fe76bf698a246e425
GNANESH N <ggnani488@gmail.com>
2025-05-15 22:36:19 +0530

    Create reverse of an array.c

---
+++ reverse of an array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n=5,a[n],b[n],i,j;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    printf("Enter elements of an array:");
+    for(i=0;i<n;i++)
+       scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+        b[(n-1)-i]=a[i];
+    printf("\nThe entered array is:");
+    for(i=0;i<n;i++)
+        printf("%3d",a[i]);
+    printf("\nThe reversed array is:");
+    for(i=0;i<n;i++)
+        printf("%3d",b[i]);
+}
