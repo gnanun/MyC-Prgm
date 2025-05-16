caa74b2537ed011ecd24c157dcf9f679d9ca7a44
GNANESH N <ggnani488@gmail.com>
2025-05-16 12:31:41 +0530

    Update reverse of an array.c

--- reverse of an array.c
+++ reverse of an array.c
@@ -1,16 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n=5,a[n],b[n],i,j;
-    printf("Enter elements of an array:");
-    for(i=0;i<n;i++)
-       scanf("%d",&a[i]);
-    for(i=0;i<n;i++)
-        b[(n-1)-i]=a[i];
+    int n=6,a[10]={2,5,8,6,4,9},i;
     printf("\nThe entered array is:");
     for(i=0;i<n;i++)
         printf("%3d",a[i]);
     printf("\nThe reversed array is:");
-    for(i=0;i<n;i++)
-        printf("%3d",b[i]);
+    for(i=n-1;i>=0;i--)
+        printf("%3d",a[i]);
 }
